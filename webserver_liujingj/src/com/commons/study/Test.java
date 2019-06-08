51588d7315cb2549d0ba1e310354f4f6dea6874b
liujingj <liujingj333@qq.com>
2019-06-08 15:46:05 +0800

    提交培训时的eclipse工程模板

---
+++ webserver_liujingj/src/com/commons/study/Test.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.commons.study;
+
+/**
+ * <p>Copyright: Copyright (c) 2019</p>
+ * <p>succez</p>
+ * @author liujingj
+ * @createdate 2019年6月8日
+ */
+public class Test {
+
+}
